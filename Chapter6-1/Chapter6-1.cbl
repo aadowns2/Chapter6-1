489e566e43a43e36fff7f9d65ba71a08fe5705f8
aadowns2 <aadowns2@gmail.com>
2016-03-18 12:44:24 -0400

    Original commit

---
+++ Chapter6-1/Chapter6-1.cbl
@@ -0,0 +1,34 @@
+       Identification Division.
+           Program-ID. Chapter6-1.
+               Author. Anthony Downs
+               Installation.
+               Date-Written. Marhc 18, 2016.
+               Date-Compiled.
+               Security.
+               
+       Environment Division.
+           Configuration Section.
+               Special-Names.
+               
+           Input-Output Section.
+               File-Control.
+               
+               I-O-Control.
+               
+       Data Division.
+           File Section.
+           
+           Working-Storage Section.
+           
+           Local-Storage Section.
+           
+           Linkage Section.
+           
+           Report Section.
+           
+       Procedure Division.
+       
+           Stop "Press <CR> to End Program"
+           Stop Run.
+           
+       End Program.
